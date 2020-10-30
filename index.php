476b196c1849fb13f36f990ed84a6e2e99f567f6
monzaa1233 <73688511+monzaa1233@users.noreply.github.com>
2020-10-30 15:37:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>นริศ พงษ์พิทักษ์</title>
+    </head>
+    <body>
+        <h1>ชื่อนริศ พงษ์พิทักษ์</h1>
+        <h1>ชื่อเล่น : Mon</h1>
+        <h2>รูปแมว001</h2>
+        <img src="cat007.jpg">
+
+        <h2>รูปแมว002</h2>
+        <img src="cat002.jpg">
+        <br>
+        <h3>เว็บที่ชอบ</h3>
+        <a href="https://www.it.kmitl.ac.th">This is a Link</a>
+
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+
+    </body>
+</html>
