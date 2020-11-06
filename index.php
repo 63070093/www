602907e40c66c86ee4cb175e5644ac0c8643aac8
63070093 <73688511+63070093@users.noreply.github.com>
2020-11-06 14:59:56 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,25 +5,29 @@
         <link rel="stylesheet" href="bootstrap-4.5.3-dist (1)/bootstrap-4.5.3-dist/css/bootstrap.min.css"/>
     </head>
     <body>
-        <h1>ชื่อนริศ พงษ์พิทักษ์</h1>
-        <h1>ชื่อเล่น : Mon</h1>
-        <h2>รูปแมว001</h2>
-        <img src="cat007.jpg">
-
-        <h2>รูปแมว002</h2>
-        <img src="cat004.jpg">
-        <br>
-        <h2>Web ที่ชอบ</h2>
-        <a href="https://www.it.kmitl.ac.th">Web นี้แหละ</a>
-
-        <h2>เรื่องตลก</h2>
-        <h3>ณ คลินิกแห่งหนึ่ง</h3>
-        <h3>หมอ:ผมมีข่าวร้ายจะมาบอก</h3>
-        <h3>คนไข้:อะไรหรอครับหมอ</h3>
-        <h3>หมอ:คุณจะอยู่ได้อีกแค่ 3 ชั่วโมง</h3>
-        <h3>คนไข้:ผมจะตายหรอครับหมอ</h3>
-        <h3>หมอ:ใช่</h3>
-        <h3>จบ(แค่นั้นแหละไม่เล่นหรอกมุขคลินิกปิดอะ)</h3>
-
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="img/banner/banner2.jpg" class="img-fluid">
+            </div>
+        </div> <!-- Close Row 1 -->
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="img/work/work1.jpg" class="img-fluid rounded" />
+                <h1>รูป2</h1>
+                <p>รูปอะไรไม่รู้</p>
+            </div>
+            <div class="col-sm-3">
+                <img src="img/work/work2.jpg" class="img-fluid img-thumbnail" />
+                <h1>Thumbnail</h1>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="col-sm-3">
+                <img src="img/work/work3.jpg" class="img-fluid rounded-circle" />
+                <h1>Circle</h1>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </div>
+        </div> <!-- Close Row 2 -->
+    </div> <!-- Close container -->
     </body>
 </html>
